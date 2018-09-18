d1a9cdc12c7abd3781a8633ba9ed05d017d2b33c
Vicky Tanzil <vicky.tanzil@gmail.com>
2018-09-18 23:00:06 +0200

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.size > 2
+    last = array.pop
+    array << "and #{last}"
+  elsif array.size == 2
+    return array.join(" and ")
+  end
+  array.join(", ")
 end
